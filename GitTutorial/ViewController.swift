bb6764ab64c5e7f9f0e4af61c6ecf4dee906a08c
hyunsik won <shik0601@gmail.com>
2020-04-12 23:18:54 +0900

    UITableView 추가

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,14 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Git test")
-            
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+
 
