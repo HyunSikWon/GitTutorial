ffe24f2909fa0f644c71ea27f9ce4628414c551f
hyunsik won <shik0601@gmail.com>
2020-05-09 20:25:37 +0900

    c++ commit

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 원현식 on 2020/05/09.
+//  Copyright © 2020 Hyunsik Won. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
